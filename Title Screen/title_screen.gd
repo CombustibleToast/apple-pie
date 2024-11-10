73070d015ce302bc3e7de379c8c53e7109b0c35d
CombustibleToast <34487106+CombustibleToast@users.noreply.github.com>
2024-11-10 11:16:17 -0500

    title screen

---
+++ Title Screen/title_screen.gd
@@ -0,0 +1,21 @@
+extends Control
+
+var jecz_time = 3.0
+var fadeout_time = 1
+@onready var elapsed_time = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if elapsed_time < jecz_time:
+		elapsed_time += delta
+	else:
+		$Button.show()
+		$Sprite2D.show()
+		$jecz.hide()
+
+func _on_button_button_up() -> void:
+	get_tree().change_scene_to_file("res://World/Game World.tscn")
