cf55b408e5872e5750dd9c06de4d67cfd65597cb
CombustibleToast <34487106+CombustibleToast@users.noreply.github.com>
2024-11-10 12:08:01 -0500

    idr but 1.0

--- Title Screen/title_screen.gd
+++ Title Screen/title_screen.gd
@@ -16,6 +16,7 @@ func _process(delta: float) -> void:
 		$Button.show()
 		$Sprite2D.show()
 		$jecz.hide()
+		$gup.hide()
 
 func _on_button_button_up() -> void:
 	get_tree().change_scene_to_file("res://World/Game World.tscn")
