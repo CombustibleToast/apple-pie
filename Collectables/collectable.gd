8fb19706c3dc63598367d23f629db1eeb5a918d3
CombustibleToast <34487106+CombustibleToast@users.noreply.github.com>
2024-11-10 09:50:15 -0500

    added yellow paint to collectables and spread them out

--- Collectables/collectable.gd
+++ Collectables/collectable.gd
@@ -2,6 +2,7 @@ extends Area3D
 
 @export var collectable_name: String
 @export var collectable_sprite: Texture2D
+var spin_rate: float = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,3 +16,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _physics_process(delta: float) -> void:
+	# Spin pointer
+	$MeshInstance3D.rotate(Vector3(0,1,0), spin_rate * delta)
